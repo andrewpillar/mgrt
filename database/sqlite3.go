56290d9c65f49c51b243d13bd73fe5f11f3ba0fe
Andrew Pillar <me@andrewpillar.com>
2019-01-12 22:15:55 +0000

    Check the error for already existing table

--- database/sqlite3.go
+++ database/sqlite3.go
@@ -1,34 +1,20 @@
 package database
 
+import "strings"
+
 func (db *DB) initSqlite3() error {
-	stmt, err := db.Prepare("SELECT COUNT(*) FROM sqlite_master WHERE type = 'table' AND name = $1")
-
-	if err != nil {
-		return err
-	}
-
-	defer stmt.Close()
-
-	var count int
-
-	row := stmt.QueryRow(db.table)
-
-	if err := row.Scan(&count); err != nil {
-		return err
-	}
-
-	if count > 0 {
-		return ErrInitialized
-	}
-
-	_, err = db.Exec(`
-		CREATE TABLE ` + db.table + ` (
+	_, err := db.Exec(`
+		CREATE TABLE mgrt_revisions (
 			id          INTEGER NOT NULL,
 			hash        BLOB NOT NULL,
 			direction   INTEGER NOT NULL,
 			created_at  TIMESTAMP NOT NULL
 		);
 	`)
+
+	if err != nil && strings.Contains(err.Error(), "already exists") {
+		return ErrInitialized
+	}
 
 	return err
 }
