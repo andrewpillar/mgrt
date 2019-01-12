c7edc0ebadee7f8c48a72ebe735c879852825691
Andrew Pillar <me@andrewpillar.com>
2019-01-12 22:29:41 +0000

    Add initMysql for revisions initialization

---
+++ database/mysql.go
@@ -0,0 +1,20 @@
+package database
+
+import "strings"
+
+func (db *DB) initMysql() error {
+	_, err := db.Exec(`
+		CREATE TABLE mgrt_revisions (
+			id         INT NOT NULL,
+			hash       BLOB NOT NULL,
+			direction  INT NOT NULL,
+			created_at TIMESTAMP NOT NULL
+		);
+	`)
+
+	if err != nil && strings.Contains(err.Error(), "already exists") {
+		return ErrInitialized
+	}
+
+	return err
+}
