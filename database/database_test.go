7e03d79b55fa1209ad45bdf8176784356f9e0a3b
Andrew Pillar <me@andrewpillar.com>
2019-03-28 19:52:39 +0000

    Add tests for PostgreSQL and MySQL
    These tests rely on environment variables being present for testing the
    specific database drivers.

--- database/database_test.go
+++ database/database_test.go
@@ -16,25 +16,9 @@ var revisionIds = []string{
 	"1136214248",
 }
 
-func TestPerform(t *testing.T) {
-	config.Root = "testdata"
-
-	cfg := &config.Config{
-		Type:    "sqlite3",
-		Address: "test.sqlite3",
-	}
-
-	db, err := Open(cfg)
-
-	if err != nil {
-		t.Errorf("failed to open sqlite3 database: %s\n", err)
-	}
-
-	defer os.Remove("test.sqlite3")
-	defer db.Close()
-
+func performRevisions(db *DB, t *testing.T) {
 	if err := db.Init(); err != nil {
-		t.Errorf("failed to initialize sqlite3 database: %s\n", err)
+		t.Errorf("failed to initialize database: %s\n", err)
 	}
 
 	performed := make([]*revision.Revision, len(revisionIds) - 1, len(revisionIds) - 1)
@@ -78,4 +62,92 @@ func TestPerform(t *testing.T) {
 			break
 		}
 	}
+}
+
+func TestPerformMySQL(t *testing.T) {
+	mysqlAddr := os.Getenv("MYSQL_ADDR")
+	mysqlUser := os.Getenv("MYSQL_USER")
+	mysqlPswd := os.Getenv("MYSQL_PSWD")
+	mysqlDb := os.Getenv("MYSQL_DB")
+
+	if mysqlAddr == "" || mysqlUser == "" || mysqlPswd == "" || mysqlDb == "" {
+		t.Log("missing one of: MYSQL_ADDR, MYSQL_USER, MYSQL_PSWD, MYSQL_DB")
+		t.Log("not running MySQL tests")
+		return
+	}
+
+	cfg := &config.Config{
+		Type:     "mysql",
+		Address:  mysqlAddr,
+		Username: mysqlUser,
+		Password: mysqlPswd,
+		Database: mysqlDb,
+	}
+
+	db, err := Open(cfg)
+
+	if err != nil {
+		t.Errorf("failed to open mysql database: %s\n", err)
+	}
+
+	defer db.Close()
+
+	performRevisions(db, t)
+}
+
+func TestPerformPostgresql(t *testing.T) {
+	pgAddr := os.Getenv("PG_ADDR")
+	pgUser := os.Getenv("PG_USER")
+	pgPswd := os.Getenv("PG_PSWD")
+	pgDb := os.Getenv("PG_DB")
+
+	if pgAddr == "" || pgUser == "" || pgPswd == "" || pgDb == "" {
+		t.Log("missing one of: PG_ADDR, PG_USER, PG_PSWD, PG_DB")
+		t.Log("not running Postgresql tests")
+		return
+	}
+
+	cfg := &config.Config{
+		Type:     "postgres",
+		Address:  pgAddr,
+		Username: pgUser,
+		Password: pgPswd,
+		Database: pgDb,
+	}
+
+	db, err := Open(cfg)
+
+	if err != nil {
+		t.Errorf("failed to open postgresql database: %s\n", err)
+	}
+
+	defer db.Close()
+
+	performRevisions(db, t)
+}
+
+func TestPerformSqlite3(t *testing.T) {
+	cfg := &config.Config{
+		Type:    "sqlite3",
+		Address: "test.sqlite3",
+	}
+
+	db, err := Open(cfg)
+
+	if err != nil {
+		t.Errorf("failed to open sqlite3 database: %s\n", err)
+	}
+
+	defer os.Remove("test.sqlite3")
+	defer db.Close()
+
+	performRevisions(db, t)
+}
+
+func TestMain(m *testing.M) {
+	config.Root = "testdata"
+
+	exitCode := m.Run()
+
+	os.Exit(exitCode)
 }
